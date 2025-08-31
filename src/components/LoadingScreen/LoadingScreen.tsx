8386c61e4042e08de1a2983ad3fd869af9830f33
CamiloBzt <73189362+CamiloBzt@users.noreply.github.com>
2025-08-30 19:38:48 -0700

    feat: brighten gold accents

--- src/components/LoadingScreen/LoadingScreen.tsx
+++ src/components/LoadingScreen/LoadingScreen.tsx
@@ -33,7 +33,7 @@ const LoadingScreen: React.FC = () => {
           >
             <path
               d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"
-              fill="#4b5d73"
+              fill="#efb810"
             />
           </svg>
           <div className={styles.pulseRing}></div>
@@ -57,7 +57,7 @@ const LoadingScreen: React.FC = () => {
           className="h-full transition-all duration-300 ease-out"
           style={{
             width: `${progress}%`,
-            background: "linear-gradient(to right, #4b5d73, #fdf6f0)",
+            background: "linear-gradient(to right, #efb810, #fdf6f0)",
           }}
         />
       </div>
